c08b04b265cba48219d671df4f26bdb2d42413c5
mrodr273 <47089416+mrodr273@users.noreply.github.com>
2019-03-01 21:36:09 -0800

    it WORKS, speeds out a list each time you click submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
+var divisible = function(numberInput) {
 
+  if (numberInput % 3 == 0 && numberInput % 5 == 0) {
+    return "pingpong"
+  } else if (numberInput % 5 == 0) {
+    return "pong"
+  } else if (numberInput % 3 == 0) {
+    return "ping"
+  } else {
+    return numberInput
+  }
+};
 $(document).ready(function() {
   $("form#number").submit(function(event) {
     event.preventDefault();
@@ -6,49 +17,26 @@ $(document).ready(function() {
     var text=""
     var data=[]
 
-    if (numberInput%3 == 0) {
-      text = "ping"
-    } else if (numberInput % 5 == 0) {
-      text = "pong"
-    } else if (numberInput % 3 == 0 && numberInput % 5 == 0) {
-      text = "pingpong"
-    } else {
-      text = numberInput
+    var arr= []
+    var arrR=[]
+    var list = "";
+    var ul = document.createElement('ul');
+    document.getElementById('myItemList').appendChild(ul);
+
+    for (var i = 0; i < numberInput; i++) {
+      var result =  divisible(numberInput);
+      numberInput -= 1
+      arr[i]= result
+      arrR.push(arr[i])
     }
-
-
-    $(".item").append(text);
-    $(".item").text(text);
+//NEED TO REVERSE ORDER OF ARRAY and display one list
     $(".col-lg-12").show();
+
+    arrR.forEach(function (arrR) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+
+    li.innerHTML += arrR;
+});
   });
 });
-
-/*$(".my-class").show();
-$(".my-class").hide();
-$(".my-class").toggle();*/
-
-/*Add a CSS class to an element:
-$("body").addClass("my-class");*/
-
-/*var add = function(number1, number2) {
-  return number1 + number2;
-};*/
-
-/*var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));*/
-
-/*$('.element-to-select').click(function() {
-  alert('hi');
-});*/
-
-/*Test that you've selected the correct DOM element:
-$('.element-to-select').css('background-color', 'green');*/
-
-/*.clickable {
-  cursor: pointer;
-  color: #0088cc;
-.clickable:hover {
-  text-decoration: underline;
-}*/
